984cda21055a4c546856af5ca4c8a248af0e0fd6
lasse <77295879+latze05@users.noreply.github.com>
2023-06-29 01:01:32 +0200

    Update index.js

--- testing/index.js
+++ testing/index.js
@@ -1,12 +1,12 @@
 const { InstatusClient } = require("../src/index");
 const client = new InstatusClient({
-  apiKey: "d20e67e75d18b1f090b73152e0d2ccc4",
+  apiKey: "",
   apiVersion: "v1",
-  pageId: "cljg3124f457533b1n3zei5boff",
+  pageId: "",
 });
 
 (async () => {
-  const getData = await client.removeTeammate("clfqw5wyp101055vgonlxz43riv");
+  const getData = await client.removeTeammate("");
 
   console.log(getData);
 })();
